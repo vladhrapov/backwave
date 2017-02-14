62f3cd727b976d98a459e1a472cc23d440a84ff5
Vlad Khrapov <vlad.hrapov@gmail.com>
2017-02-14 18:55:11 +0200

    Add redux observable and rxjs

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import 'rxjs';
 
 import Root from "./components/Root.jsx";
 
